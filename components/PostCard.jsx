306648dd1f2229b744b6cfb13d112713314a3ea3
Winston Pool <winston.pool@nike.com>
2022-03-20 21:37:30 +0900

    fixed trailing spaces

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -29,18 +29,18 @@ const PostCard = ({ post }) => (
       </div>
       <div className="font-medium text-gray-700 mb-1">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
         </svg>
         <span>
-        {moment(post.createdAt).format('MMM DD, YYYY')}
+          {moment(post.createdAt).format('MMM DD, YYYY')}
         </span>
       </div>
       <p className="text-center text-base text-gray-700 font-normal px-4 lg:pd-20 mb-6">{post.excerpt}</p>
       <div className="text-center">
         <Link href={`/post/${post.slug}`}>
-        <span className="transition duration-400 transform hover:-translate-y-1 hover:bg-yellow-500 inline-block bg-gray-200 text-lg font-medium rounded-full text-gray-500 hover:text-white px-8 py-2 cursor-pointer">
+          <span className="transition duration-400 transform hover:-translate-y-1 hover:bg-yellow-500 inline-block bg-gray-200 text-lg font-medium rounded-full text-gray-500 hover:text-white px-8 py-2 cursor-pointer">
             続きを読む
-        </span>
+          </span>
         </Link>
       </div>
     </div>
