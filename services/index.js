d6d9cacd00dcfbf0ffafee7e41b0078714079114
Winston Pool <winston.pool@nike.com>
2022-03-21 17:10:13 +0900

    changed ordering of getCategoryPost

--- services/index.js
+++ services/index.js
@@ -149,7 +149,10 @@ export const getAdjacentPosts = async (createdAt, slug) => {
 export const getCategoryPost = async (slug) => {
   const query = gql`
     query GetCategoryPost($slug: String!) {
-      postsConnection(where: {categories_some: {slug: $slug}}) {
+      postsConnection(
+        where: {categories_some: {slug: $slug}}
+        orderBy: createdAt_DESC
+        ) {
         edges {
           cursor
           node {
